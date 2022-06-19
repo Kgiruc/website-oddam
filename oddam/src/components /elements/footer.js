1a706a9d065c4376745a891fc2568d6d51dfc1be
Kgiruc <kgiruc76@gmail.com>
2022-06-19 22:01:37 +0200

    changes styles label

--- oddam/src/components /elements/footer.js
+++ oddam/src/components /elements/footer.js
@@ -10,20 +10,29 @@ export default function Footer(){
         <>
             <div className="container__footer">
                 <div className="background__footer">
-                    <div className="container__footer__form">
-                        <span>Skontaktuj się z nami </span>
-                        <img src={Decoration} alt="dekoracja"/>
-                        <input/>
-                        <input/>
-                        <input/>
-                        <button>Wyślij</button>
+                    <div className="container__footer__subtitles">
+                        <span>Skontatuj się z nami</span>
+                        <img  className="img__decoration__footer" src={Decoration} alt="decoration"/>
+                        <form className="container__footer__subtitles__form">
+                            <label>
+                                Wpisz swoje imię
+                                <input type="text"  />
+                                Wpisz swój email
+                                <input type="text"  />
+                                Wpisz swoją wiadomość
+                                <input type="text"  />
+                            </label>
+                            <button>Wyślij</button>
+                        </form>
                     </div>
-                        <ul className="footer__subs">
-                        <span className="footer__subs">Copyright by Coders Lab</span>
-                        <img src={Instagram} alt="instagram"/>
-                        <img src={Facebook} alt="facebook"/>
-                        </ul>
-                </div>
+                    </div>
+
+                        <div className="footer__subs">
+                            <span className="footer__subs">Copyright by Coders Lab</span>
+                            <img src={Facebook} alt="facebook"/>
+                            <img src={Instagram} alt="instagram"/>
+
+                        </div>
             </div>
         </>
     )
