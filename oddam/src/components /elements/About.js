84410cb057457e92ee9d19675d1ad97d9608d599
Kgiruc <kgiruc76@gmail.com>
2022-06-27 13:33:40 +0200

    changes in code style

--- oddam/src/components /elements/About.js
+++ oddam/src/components /elements/About.js
@@ -2,19 +2,17 @@ import React from "react";
 import Decoration from "../../assets/Decoration.svg"
 import Signature from "../../assets/Signature.svg"
 
-
-export default function About(){
-    return(
+export default function About() {
+    return (
         <div className="container__about" id="about">
             <div className="container__about__1">
                 <span>O nas</span>
-                <img src={Decoration} alt="decoration" />
+                <img src={Decoration} alt="decoration"/>
                 <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery.
                     Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-                <img className="signature" src={Signature} alt="signature" />
+                <img className="signature" src={Signature} alt="signature"/>
             </div>
             <div className="container__about__2">
-
             </div>
         </div>
     )
--- oddam/src/components /elements/footer.js
+++ oddam/src/components /elements/footer.js
@@ -1,20 +1,14 @@
-import React from "react";
 import Decoration from "../../assets/Decoration.svg"
 import Instagram from "../../assets/Instagram.svg"
 import Facebook from "../../assets/Facebook.svg"
 
-
-
-
 export default function Footer(){
     return(
-        <>
-            <div className="container__footer" id="footer">
+            <footer className="container__footer" id="footer">
                 <div className="background__footer">
                     <div className="container__footer__subtitles">
                         <span>Skontatuj się z nami</span>
                         <img  className="img__decoration__footer" src={Decoration} alt="decoration"/>
-
                         <form className="container__footer__subtitles__form">
                             <div className="container__footer__subtitles__form__row">
                                 <span>
@@ -22,8 +16,8 @@ export default function Footer(){
                                     <input  placeholder="Krzysztof" type="text"/>
                                 </span>
                                 <span>
-                                <label>Wpisz swój email</label>
-                                <input placeholder="abc@xyz.pl" type="email" id="email" />
+                                    <label>Wpisz swój email</label>
+                                    <input placeholder="abc@xyz.pl" type="email" id="email" />
                                 </span>
                             </div>
                             <div className="container__footer__subtitles__form__message">
@@ -41,14 +35,11 @@ export default function Footer(){
                         </form>
                     </div>
                     </div>
-
                         <div className="footer__subs">
                             <span className="footer__subs">Copyright by Coders Lab</span>
                             <img src={Facebook} alt="facebook"/>
                             <img src={Instagram} alt="instagram"/>
-
                         </div>
-            </div>
-        </>
+            </footer>
     )
 }
