248e8dce47ece650e42430f626caac3e3e84c288
Kgiruc <kgiruc76@gmail.com>
2022-07-14 14:13:54 +0200

    media  in code style

--- oddam/src/components /Home.js
+++ oddam/src/components /Home.js
@@ -10,7 +10,7 @@ export default function Home() {
     return (
         <>
             <Navigation/>
-            {/*<ThreeColumns/>*/}
+            <ThreeColumns/>
             {/*<SimpleSteps/>*/}
             {/*<About/>*/}
             {/*<Help/>*/}
